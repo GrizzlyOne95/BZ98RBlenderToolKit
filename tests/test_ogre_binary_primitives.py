fea72ff9b02efd51fab187326009e8db3ef8382c
GrizzlyOne95 <143971298+GrizzlyOne95@users.noreply.github.com>
2026-09-16 11:16:46 -0500

    Make Ogre primitive tests unittest-discoverable

--- tests/test_ogre_binary_primitives.py
+++ tests/test_ogre_binary_primitives.py
@@ -1,20 +1,26 @@
+import unittest
+
 from bz98tools.ogrefast.pure.binary import BinaryWriter
 
 
-def test_chunk_length_is_backpatched():
-    writer = BinaryWriter()
-    with writer.chunk(0x3000):
-        writer.u16(0x1234)
-        writer.u32(0x56789ABC)
+class OgreBinaryPrimitiveTests(unittest.TestCase):
+    def test_chunk_length_is_backpatched(self):
+        writer = BinaryWriter()
+        with writer.chunk(0x3000):
+            writer.u16(0x1234)
+            writer.u32(0x56789ABC)
 
-    data = writer.getvalue()
-    assert data[:2] == b"\x00\x30"
-    assert int.from_bytes(data[2:6], "little") == len(data)
-    assert data[6:8] == b"\x34\x12"
-    assert data[8:12] == b"\xbc\x9a\x78\x56"
+        data = writer.getvalue()
+        self.assertEqual(data[:2], b"\x00\x30")
+        self.assertEqual(int.from_bytes(data[2:6], "little"), len(data))
+        self.assertEqual(data[6:8], b"\x34\x12")
+        self.assertEqual(data[8:12], b"\xbc\x9a\x78\x56")
+
+    def test_line_is_newline_terminated_utf8(self):
+        writer = BinaryWriter()
+        writer.line("[MeshSerializer_v1.100]")
+        self.assertEqual(writer.getvalue(), b"[MeshSerializer_v1.100]\n")
 
 
-def test_line_is_newline_terminated_utf8():
-    writer = BinaryWriter()
-    writer.line("[MeshSerializer_v1.100]")
-    assert writer.getvalue() == b"[MeshSerializer_v1.100]\n"
+if __name__ == "__main__":
+    unittest.main()
