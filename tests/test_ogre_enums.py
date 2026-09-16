8943a4616c83eb7bcd304cf5a1a5e7280ee76313
GrizzlyOne95 <143971298+GrizzlyOne95@users.noreply.github.com>
2026-09-16 11:16:53 -0500

    Make Ogre enum tests unittest-discoverable

--- tests/test_ogre_enums.py
+++ tests/test_ogre_enums.py
@@ -1,3 +1,5 @@
+import unittest
+
 from bz98tools.ogrefast.pure.enums import (
     OperationType,
     VertexElementSemantic,
@@ -7,14 +9,20 @@ from bz98tools.ogrefast.pure.enums import (
 )
 
 
-def test_core_ogre_enum_values():
-    assert VertexElementType.FLOAT3 == 2
-    assert VertexElementSemantic.POSITION == 1
-    assert VertexElementSemantic.NORMAL == 4
-    assert VertexElementSemantic.TEXTURE_COORDINATES == 7
-    assert OperationType.TRIANGLE_LIST == 4
+class OgreEnumTests(unittest.TestCase):
+    def test_core_ogre_enum_values(self):
+        self.assertEqual(VertexElementType.FLOAT3, 2)
+        self.assertEqual(VertexElementSemantic.POSITION, 1)
+        self.assertEqual(VertexElementSemantic.NORMAL, 4)
+        self.assertEqual(VertexElementSemantic.TEXTURE_COORDINATES, 7)
+        self.assertEqual(OperationType.TRIANGLE_LIST, 4)
+
+    def test_string_enum_mapping(self):
+        self.assertEqual(vertex_type("float3"), VertexElementType.FLOAT3)
+        self.assertEqual(
+            vertex_semantic("texcoord"), VertexElementSemantic.TEXTURE_COORDINATES
+        )
 
 
-def test_string_enum_mapping():
-    assert vertex_type("float3") == VertexElementType.FLOAT3
-    assert vertex_semantic("texcoord") == VertexElementSemantic.TEXTURE_COORDINATES
+if __name__ == "__main__":
+    unittest.main()
